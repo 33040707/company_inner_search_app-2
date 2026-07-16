d2d390dcefc753d3103776763e3d9e279e97ed68
Akira <piece.oo.clover8@gmail.com>
2026-07-16 17:51:43 +0900

    first commit.

--- main.py
+++ main.py
@@ -5,14 +5,8 @@
 ############################################################
 # 1. ライブラリの読み込み
 ############################################################
-# --- [修正] python-dotenvがない場合でもアプリを止めない安全弁 ---
-try:
-    from dotenv import load_dotenv
-    load_dotenv()
-except ImportError:
-    # Streamlit Cloud等でインストールに失敗していても、エラーにせずスルーする
-    pass
-
+# 「.env」ファイルから環境変数を読み込むための関数
+from dotenv import load_dotenv
 # ログ出力を行うためのモジュール
 import logging
 # streamlitアプリの表示を担当するモジュール

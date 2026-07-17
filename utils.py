6b69122caa46b475a66bea63e62454a0aeccb666
Akira <piece.oo.clover8@gmail.com>
2026-07-17 14:23:05 +0900

    first commit.

--- utils.py
+++ utils.py
@@ -4,12 +4,15 @@
 
 import os
 import streamlit as st
-# 👇 以下の2行を langchain_core からインポートするように変更しました
+
+# 👇 LangChain関連のインポートを、最新バージョンの構造に合わせて個別に直接指定しました
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
-from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
+
 import constants as ct
 
 def get_source_icon(source):

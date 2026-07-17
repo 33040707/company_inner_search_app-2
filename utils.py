d8122d934cec36db944c5a06c2132f1ae23db42e
Akira <piece.oo.clover8@gmail.com>
2026-07-17 14:18:53 +0900

    first commit.

--- utils.py
+++ utils.py
@@ -4,8 +4,9 @@
 
 import os
 import streamlit as st
-from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain.schema import HumanMessage
+# 👇 以下の2行を langchain_core からインポートするように変更しました
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
